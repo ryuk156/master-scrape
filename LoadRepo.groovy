3b99fccc2687917aaab934f6cf44bc43f3d5a10b
Yash Patel <49101492+ryuk156@users.noreply.github.com>
2021-05-28 13:42:08 +0530

    Update LoadRepo.groovy

--- LoadRepo.groovy
+++ LoadRepo.groovy
@@ -1,18 +1,17 @@
 #!/usr/bin/env groovy
+package org.kohsuke.github
+@Grab(group='org.kohsuke', module='github-api', version='1.75')
+import org.kohsuke.github.GitHub
+
 def fetch() {
-	
-	def response = sh(script: 'curl -s https://api.github.com/orgs/terasology/repos', returnStdout: true).trim()
-	def json = new groovy.json.JsonSlurperClassic().parseText(response)
-        def repos=[]
+	def repos = []
+	def org = 'Terasology';
+	def githubCom = GitHub.connectUsingOAuth('ddae1e41099bb89a636241818107dff969c27695');
 
-   json.each {
-       repos << it.name
+	githubCom.getOrganization(org).listRepositories().each {
+		repos << it.getName()
 	}
-	
 	return repos
-
-
-		
 }
 
 return this
